3e34bd0591ba46561e9742982db16cde0f0974ec
Herschel Gonzalez <contacto@herschelgonzalez.com>
2023-01-21 00:55:58 -0600

    se agrego menu principal con casos de uso principales

--- app/src/main/java/com/herschel/cajarapida/view/home/HomeActivity.kt
+++ app/src/main/java/com/herschel/cajarapida/view/home/HomeActivity.kt
@@ -6,10 +6,14 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.basgeekball.awesomevalidation.AwesomeValidation
 import com.basgeekball.awesomevalidation.ValidationStyle
+import com.example.cajarapida.R
 import com.example.cajarapida.databinding.ActivityHomeBinding
 import com.example.cajarapida.databinding.ActivityMainBinding
+import com.herschel.cajarapida.view.products.ProductsFragment
+import com.herschel.cajarapida.view.stadistics.StadisticsFragment
 import com.herschel.cajarapida.viewmodel.LoginViewModel
 
 class HomeActivity : AppCompatActivity() {
@@ -17,16 +21,36 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        reemplazarFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.ventas -> reemplazarFragment(HomeFragment())
+                R.id.productos -> reemplazarFragment(ProductsFragment())
+                R.id.estadisticas -> reemplazarFragment(StadisticsFragment())
+
+                else ->{
+
+                }
+
+
+            }
+            true
+        }
 
     }
 
 
-
+private fun reemplazarFragment(fragment : Fragment){
+    val fragmentManager = supportFragmentManager
+    val fragmentTransaction = fragmentManager.beginTransaction()
+    fragmentTransaction.replace(R.id.frame_layout,fragment)
+    fragmentTransaction.commit()
+}
 
 }
